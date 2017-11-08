382b4322e734b1ec093958e44783ff99cae649be
atilapaes <atila.paes@gmail.com>
2017-11-07 19:32:50 -0700

    Function inclusion: rotate2ray_center
    This function rotates the gph signal from ZH1H2 to LQT coordinates

--- 8_shared_modules/om_general_signal_processing.py
+++ 8_shared_modules/om_general_signal_processing.py
@@ -968,4 +968,71 @@ def _plot(x, mph, mpd, threshold, edge, valley, ax, ind):
         # plt.grid()
         plt.show()
 
- #%%
+#%%
+def rotate2ray_center(gph_data,print_results=False,print_plot=False,gph_number='No Info'):
+    """
+    This function rotates the gph signal from ZH1H2 to LQT coordinates
+    """
+    import numpy
+    import matplotlib.pyplot as plt
+    from obspy.signal.polarization import particle_motion_odr
+    from obspy.signal.rotate import rotate_zne_lqt
+   
+    rotated_gph_data=gph_data.copy()
+    time=numpy.linspace(gph_data[0].stats.starttime.second, gph_data[0].stats.endtime.second,gph_data[0].stats.npts)
+    
+    # Calculating angles for non-null arrays
+    if ((numpy.sum(gph_data[0].data) !=0) and (numpy.sum(gph_data[1].data)!=0) and (numpy.sum(gph_data[2].data)!=0)):
+        odr_azimuth, odr_incidence, odr_azi_error, odr_inc_error=particle_motion_odr(gph_data)
+
+        # Correcting angles
+        inclination=90-odr_incidence
+        if odr_azimuth <180:
+            ba=odr_azimuth+180
+        else:
+            ba=odr_azimuth-180
+
+        # Print results for debugging
+        if print_results==True:
+            print('Gph_number: '+str(gph_number)+'. BA: ',str(round(ba,2)),'. Inclination: ', str(round(inclination,2)))
+ 
+        # Signal Rotation
+        rotated_gph_data[0].data, rotated_gph_data[1].data, rotated_gph_data[2].data=rotate_zne_lqt(gph_data[0].data, gph_data[1].data, gph_data[2].data, ba=ba, inc=inclination)
+    else:
+        print('Null array identified')
+   
+    if print_plot==True:
+        plt.figure(1,figsize=(8,5))
+        
+        plt.subplot(3,2,1)
+        plt.title('Original data- Gph '+str(gph_number))
+        plt.ylabel('Z', weight='bold')
+        plt.plot(time,gph_data[0].data,lw=0.5,c='r')
+        
+        plt.subplot(3,2,3)
+        plt.ylabel('h1', weight='bold')
+        plt.plot(time,gph_data[1].data,lw=0.5,c='g')
+        
+        plt.subplot(3,2,5)
+        plt.ylabel('h2', weight='bold')
+        plt.plot(time,gph_data[2].data,lw=0.5,c='b')
+                
+        plt.subplot(3,2,2)
+        plt.title('Rotated. BA: '+str(round(ba,2))+'. Inc: '+str(round(inclination,2)))
+        plt.ylabel('L', weight='bold')
+        plt.plot(time,rotated_gph_data[0].data,c='k',lw=0.5)
+        
+        plt.subplot(3,2,4)
+        plt.ylabel('Q', weight='bold')
+        plt.plot(time,rotated_gph_data[1].data,lw=0.5,c='k')
+        
+        plt.subplot(3,2,6)
+        plt.ylabel('T', weight='bold')
+        plt.plot(time,rotated_gph_data[2].data,lw=0.5,c='k')
+        plt.tight_layout()
+        
+        plt.show()
+        plt.close()
+        
+    return(rotated_gph_data)
+#%%
