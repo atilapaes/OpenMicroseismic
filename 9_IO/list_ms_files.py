ee9602d476ad44a28e3330d8c3ee224317692803
atilapaes <atila.paes@gmail.com>
2017-12-13 01:40:42 -0700

    Include list_ms_files
    This script creates the CSV file containing the file names and start
    time. It is one of the I/O project files.

---
+++ 9_IO/list_ms_files.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 18:34:48 2017
+
+@author: atilapaes
+
+
+"""
+
+dataset_name='sample'
+files_path='Files/'
+extension='dat'
+
+def output_file_datetime(dataset_name,files_path,extension):
+    """
+    This function generates the file contaning the file names and its start times
+    """
+    import obspy, pandas
+    from os import listdir
+    from os.path import isfile, join
+    ms_files = [f for f in listdir(files_path) if isfile(join(files_path, f)) and f.endswith("."+extension)]
+
+    starttime=[]
+    for index in range(len(ms_files)):
+        ms_data=obspy.read(files_path+ms_files[index])
+        starttime.append(ms_data[0].stats.starttime)
+
+    file_time_df=pandas.DataFrame({'file_name':ms_files,'date_time':starttime})
+    file_time_df=file_time_df.sort_values('date_time')
+    file_time_df.to_csv(dataset_name+'.csv', sep=',', line_terminator='\n', index=False)
+
+
+# Importing 
+#sample=pandas.read_csv('sample.csv')
+#sample['date_time'] = list(map(lambda x: obspy.core.utcdatetime.UTCDateTime(x), sample.date_time))
